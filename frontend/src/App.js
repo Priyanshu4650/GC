d2eb4f7e17935e27aad209a22a78d72b2ed66780
Priyanshu4650 <priyanshu022017@gmail.com>
2025-02-24 12:04:34 +0530

    checking for not found

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Events from "./pages/Events";
 import Navbar from "./components/Navbar";
@@ -13,14 +13,14 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home title="Home" />} />
-          <Route path="/events" element={<Events title="Events" />} />
+          <Route path="#/" element={<Home title="Home" />} />
+          <Route path="#/events" element={<Events title="Events" />} />
           <Route
-            path="/points-table"
+            path="#/points-table"
             element={<SportsPointsTable title="Leaderboard" />}
           />
-          <Route path="/login" element={<AdminPanel title="Admin Page" />} />
-          <Route path="/team" element={<Team title="The Team" />} />
+          <Route path="#/login" element={<AdminPanel title="Admin Page" />} />
+          <Route path="#/team" element={<Team title="The Team" />} />
           <Route path="*" element={<Home title="Home" />} />
           {/* <Route path="/previous" element={<PointsTable />}/> */}
         </Routes>
